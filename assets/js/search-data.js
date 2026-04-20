3da3adb8adf13129f0de15d96d7342ae0c02a8a8
jeonwonje <57317338+jeonwonje@users.noreply.github.com>
2026-04-20 15:23:45 +0000

    Deploying to gh-pages from @ jeonwonje/CDE3301-ASI301@556aaeb0e251fd027aba5c6fa7540d060a0581f8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,15 +3,15 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-cde3301-sand-collection-system",
-    title: "CDE3301 · Sand Collection System",
+    id: "nav-cde3301-beach-debris-collection-system",
+    title: "CDE3301 · Beach Debris Collection System",
     section: "Navigation",
     handler: () => {
       window.location.href = "/CDE3301-ASI301/";
     },
   },{id: "nav-gantt-chart",
           title: "Gantt Chart",
-          description: "Interactive Gantt chart for the Sand Collection System project (CDE3301 / ASI-301).",
+          description: "Interactive Gantt chart for the Beach Collection System project (CDE3301 / ASI-301).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/CDE3301-ASI301/gantt/";
